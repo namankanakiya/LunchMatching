a5fbe3add9c61f8182fa9ad15d1d4df986c62d45
Naman Kanakiya <nakanaki@microsoft.com>
2019-03-25 16:23:02 -0700

    Adding sessions to express

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,9 @@ require("dotenv").config();
 require("./server/db-conn");
 const express = require("express");
 const bodyParser = require("body-parser");
-const fetch = require("node-fetch");
+const {isAuthenticated} = require("./server/auth/auth")
 
 const app = express();
-const graphUrl = "https://graph.microsoft.com/v1.0/";
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -24,37 +23,3 @@ app.get("/*", (req, res) => {
 
 const { PORT } = process.env;
 app.listen(PORT, () => console.log(`Something happening on ${PORT}`));
-
-function isAuthenticated(req, res, next) {
-  const headers = { Authorization: req.headers.authorization };
-  const options = {
-    headers: headers
-  };
-  fetch(`${graphUrl}/me`, options)
-    .then(checkStatus)
-    .then(response => {
-      return response.json();
-    })
-    .then(
-      data => {
-        res.locals.user = data;
-        next();
-      },
-      error => {
-        next(error);
-      }
-    )
-    .catch(err => {
-      next(err);
-    });
-}
-
-function checkStatus(res) {
-  if (res.ok) {
-    return res;
-  } else {
-    var err = new Error(res.statusText);
-    err.status = res.status;
-    throw err;
-  }
-}
---
+++ backend/server/auth/auth.js
@@ -0,0 +1,38 @@
+const fetch = require("node-fetch");
+const graphUrl = "https://graph.microsoft.com/v1.0/";
+
+const isAuthenticated = (req, res, next) => {
+  const headers = { Authorization: req.headers.authorization };
+  const options = {
+      headers: headers
+  };
+  fetch(`${graphUrl}/me`, options)
+      .then(checkStatus)
+      .then(response => {
+      return response.json();
+      })
+      .then(
+      data => {
+          res.locals.user = data;
+          next();
+      },
+      error => {
+          next(error);
+      }
+      )
+      .catch(err => {
+      next(err);
+      });
+}
+  
+const checkStatus = (res) => {
+  if (res.ok) {
+    return res;
+  } else {
+    var err = new Error(res.statusText);
+    err.status = res.status;
+    throw err;
+  }
+}
+
+module.exports = {isAuthenticated, checkStatus}
